007ee5a5c5a6f21b63b1fb65d9099bb507703a90
Ashley Lerma <lerma.scout@gmail.com>
2020-06-30 21:10:13 -0500

    app.use

--- server1.js
+++ server1.js
@@ -5,6 +5,10 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Data
 // ===========================================================
 var characters = [
